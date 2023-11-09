a6990049e484c013d16c0c4aa5a8d08a91c0e032
xing.dong <xing.dong@appshahe.com>
2023-11-09 15:21:29 +0800

    feat:添加事件

--- MyMMO/Assets/Scripts/Event/EventPool.cs
+++ MyMMO/Assets/Scripts/Event/EventPool.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Tools;
+using UnityEngine.XR;
 
 public sealed partial class EventPool<T> where T : BaseEventArgs
 {
     private Dictionary<int, EventHandler<T>> eventHandlers;
-    private Queue<Event> events;
-    private Dictionary<object, LinkedListNode<EventHandler<T>>> cacheNodes;
-    private Dictionary<object, LinkedListNode<EventHandler<T>>> tempNodes;
-    private EventPoolMode eventPoolMode;
-    private EventHandler<T> defalutHandler;
+    private Queue<Event> events;   
     /// <summary>
     /// 事件池初始化
     /// </summary>
@@ -21,11 +18,95 @@ public sealed partial class EventPool<T> where T : BaseEventArgs
     {
         eventHandlers = new Dictionary<int, EventHandler<T>>();
         events = new Queue<Event>();
-        cacheNodes = new Dictionary<object, LinkedListNode<EventHandler<T>>>();
-        tempNodes = new Dictionary<object, LinkedListNode<EventHandler<T>>>();
-        eventPoolMode = mode;
-        defalutHandler = null;
+       
+    }
+    /// <summary>
+    /// 事件池轮询
+    /// </summary>
+    public void Update()
+    {
+        lock (events)
+        {
+            while (events.Count > 0)
+            {
+                Event eventNode = events.Dequeue();
+                HandleEvent(eventNode.Sender, eventNode.EventArgs);
+                ReferencePool.Release(eventNode);
+            }
+        }
     }
 
+    /// <summary>
+    /// 处理事件节点
+    /// </summary>
+    /// <param name="sender">事件源</param>
+    /// <param name="e">事件参数</param>
+    private void HandleEvent(object sender, T e)
+    {
+        if(eventHandlers.TryGetValue(e.Id,out var handler))
+        {
+            handler(sender, e);
+            ReferencePool.Release(e);
+        }
+    }
+
+    /// <summary>
+    /// 检查是否存在事件处理函数
+    /// </summary>
+    /// <param name="id">事件类型编号</param>   
+    /// <returns></returns>
+    public bool Check(int id)
+    {
+        return eventHandlers.ContainsKey(id);
+    }
+
+    /// <summary>
+    /// 订阅事件处理函数
+    /// </summary>
+    /// <param name="id">事件类型编号</param>
+    /// <param name="handler">要订阅的事件处理函数</param>
+    public void Subscribe(int id, EventHandler<T> handler)
+    {
+        if (!eventHandlers.ContainsKey(id))
+        {
+            eventHandlers.Add(id, handler);
+        }
+    }
+
+    /// <summary>
+    /// 取消订阅事件处理函数
+    /// </summary>
+    /// <param name="id"></param>
+    public void Unsubscribe(int id)
+    {
+        if (!eventHandlers.Remove(id))
+        {
+            LogTool.LogErrorFormat("Event '{0}' not exists specified handler.", id);
+        }
+    }
+
+    /// <summary>
+    /// 抛出事件，这个操作是线程安全的，即使不在主线程中抛出，也可保证在主线程中回调事件处理函数，但事件会在抛出后的下一帧分发。
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public void Fire(object sender,T e)
+    {
+        Event eventNode=Event.Create(sender, e);
+        lock (events)
+        {
+            events.Enqueue(eventNode);
+        }
+    }
+
+    /// <summary>
+    /// 抛出事件立即模式，这个操作不是线程安全的，事件会立刻分发。
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public void FireNow(object sender, T e)
+    {
+        HandleEvent(sender, e);
+    }
 }
 
